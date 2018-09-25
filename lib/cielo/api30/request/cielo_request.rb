76dea936e9f9ccb99a0656ac954442656ff607d7
Henrique Feitosa <hfeitosa@grupoea.com.br>
2018-09-25 14:13:14 -0300

    Setted debug option on

--- lib/cielo/api30/request/cielo_request.rb
+++ lib/cielo/api30/request/cielo_request.rb
@@ -32,6 +32,7 @@ module Cielo
 
           client = Net::HTTP.new(uri.host, uri.port)
           client.use_ssl = true
+          client.set_debug_output($stdout)
 
           response = client.send_request(method, uri.request_uri, body, headers)
 
